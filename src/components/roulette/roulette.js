bdf2d3c5c85f74d54e53be15757e1492a6892821
Nick <ncapito@gmail.com>
2014-11-02 09:25:14 -0500

    played around with kineticjs

--- src/components/roulette/roulette.js
+++ src/components/roulette/roulette.js
@@ -38,7 +38,11 @@
       wheel.update();
       $scope.$watch('game.display()', function() {
         wheel.update();
+        kineticjs.draw();
       });
+      kineticjs.segments = game.letters;
+      kineticjs.init();
+      kineticjs.draw();
     }, 1000);
 
     function Game(data) {
@@ -99,30 +103,134 @@
 
     }
 
+    var PI2 = Math.PI * 2;
+    var color={
+      colors: {},
+      getRandomColor:function () {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      },
+      getColor: function(letter){
+          //check if it exists
+          var exists  = color.colors[letter];
+          if(exists){
+            return exists;
+          }
+          if(letter === ''){
+            exists = color.colors[letter] = '#ffffff';
+          }else{
+            exists = color.colors[letter] = color.getRandomColor();
+          }
+          return exists;
+      },
+    }
 
-    Number.prototype.mod = function(n) {
-      return ((this % n) + n) % n;
-    };
+    var kineticjs = {
+      size: 290,
+
+      segments: [],
+      radius: 290,
+      width: 600,
+      height: 600,
+      stage: null,
+      init: function(){
+        if(kineticjs.stage){
+          return;
+        }
+
+        kineticjs.stage = new Kinetic.Stage({
+          container: 'canvas2',
+          width: kineticjs.width,
+          height: kineticjs.height,
+          display: 'block'
+        });
+      },
+
+      draw: function(){
+        kineticjs.stage.clear();
+        var layer = new Kinetic.Layer();
+
+        var center = new Kinetic.Circle({
+            x: 300,
+            y: 300,
+            radius: 150,
+            fill: 'white',
+            stroke: 'black'
+        });
+        layer.add(center);
+
+        var startingAngle = 0;
+        var rotatingAngle = startingAngle;
+        var angleOffset =  360/ kineticjs.segments.length;
+        angular.forEach(kineticjs.segments, function(item){
+            var value = item.letter || '';
+            rotatingAngle += angleOffset;
+            var wedge = new Kinetic.Wedge({
+              x: kineticjs.stage.width() / 2,
+              y: kineticjs.stage.height() / 2,
+              radius: kineticjs.radius,
+              angle: angleOffset,
+              fill: color.getColor(value),
+              stroke: 'black',
+              strokeWidth: 4,
+              rotation: rotatingAngle
+
+            });
+            layer.add(wedge);
+            wedge.moveToBottom();
+
+
+            var offset = {
+                x: -(kineticjs.radius) + 75, //innercirl is 150,
+                y: -30
+            };
+            var textAngle = rotatingAngle;
+
+
+            if (rotatingAngle > 90 && rotatingAngle < 270) {
+                var offset = {
+                    x: kineticjs.radius - 75,
+                    y: 50
+                };
+                var textAngle = rotatingAngle - 180;
+
+            }
+
+            var text = new Kinetic.Text({
+                  x:300,
+                  y:300,
+                  text: value,
+                  fontSize: 20,
+                  fill: 'black',
+                  rotationDeg:textAngle,
+                  offset:offset
+              });
+            layer.add(text);
+
+
+        });
+        // add the layer to the stage
+        kineticjs.stage.add(layer);
+        layer.draw();
+
+      }
+    }
+
+
 
 
     // WHEEL!
     var wheel = {
-
-      timerHandle: 0,
-      timerDelay: 33,
       angleCurrent: 0,
-      angleDelta: 0,
       size: 290,
       canvasContext: null,
-      colors: ['#ffff00', '#ffc700', '#ff9100', '#ff6301', '#ff0000', '#c6037e',
-        '#713697', '#444ea1', '#2772b2', '#0297ba', '#008e5b', '#8ac819'
-      ],
+
       segments: [],
       segColors: [], // Cache of segments to colors
-      maxSpeed: Math.PI / 16,
-      upTime: 1000, // How long to spin up for (in ms)
-      downTime: 17000, // How long to slow down for (in ms)
-      spinStart: 0,
       frames: 0,
       centerX: 300,
       centerY: 300,
@@ -159,20 +267,6 @@
         //var r = Math.floor(Math.random() * wheel.segments.length);
         var r = 0;
         wheel.angleCurrent = ((r + 0.5) / wheel.segments.length) * Math.PI * 2;
-
-        var segments = wheel.segments;
-        var len = segments.length;
-        var colors = wheel.colors;
-        var colorLen = colors.length;
-
-        // Generate a color cache (so we have consistant coloring)
-        var segColor = [];
-        for (var i = 0; i < len; i++){
-          segColor.push(colors[segments[i].hashCode().mod(colorLen)]);
-        }
-
-        wheel.segColor = segColor;
-
         wheel.draw();
       },
 
@@ -188,54 +282,37 @@
 
       drawSegment: function(key, lastAngle, angle) {
         var ctx = wheel.canvasContext;
-        var centerX = wheel.centerX;
-        var centerY = wheel.centerY;
-        var size = wheel.size;
-
-        var segments = wheel.segments;
-        var colors = wheel.segColor;
-
-        var value = segments[key].letter || '';
+        var value = wheel.segments[key].letter || '';
 
         ctx.save();
         ctx.beginPath();
 
         // Start in the centre
-        ctx.moveTo(centerX, centerY);
-        ctx.arc(centerX, centerY, size, lastAngle, angle, false); // Draw a arc around the edge
-        ctx.lineTo(centerX, centerY); // Now draw a line back to the centre
+        ctx.moveTo(wheel.centerX, wheel.centerY);
+        ctx.arc(wheel.centerX, wheel.centerY, wheel.size, lastAngle, angle, false); // Draw a arc around the edge
+        ctx.lineTo(wheel.centerX, wheel.centerY); // Now draw a line back to the centre
         // Clip anything that follows to this area
         //ctx.clip(); // It would be best to clip, but we can double performance without it
         ctx.closePath();
 
-        ctx.fillStyle = colors[key];
+        ctx.fillStyle = color.getColor(value);
         ctx.fill();
         ctx.stroke();
 
         // Now draw the text
         ctx.save(); // The save ensures this works on Android devices
-        ctx.translate(centerX, centerY);
+        ctx.translate(wheel.centerX, wheel.centerY);
         ctx.rotate((lastAngle + angle) / 2);
 
         ctx.fillStyle = '#000000';
-        ctx.fillText(value.substr(0, 20), size / 2 + 100, 0);
+        ctx.fillText(value.substr(0, 20), wheel.size / 2 + 100, 0);
         ctx.restore();
 
       },
 
       drawWheel: function() {
         var ctx = wheel.canvasContext;
-
-        var angleCurrent = wheel.angleCurrent;
-        var lastAngle = angleCurrent;
-
-        var len = wheel.segments.length;
-
-        var centerX = wheel.centerX;
-        var centerY = wheel.centerY;
-        var size = wheel.size;
-
-        var PI2 = Math.PI * 2;
+        var lastAngle = wheel.angleCurrent;
 
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#000000';
@@ -243,14 +320,14 @@
         ctx.textAlign = 'center';
         ctx.font = '1.4em Arial';
 
-        for (var i = 1; i <= len; i++) {
-          var angle = PI2 * (i / len) + angleCurrent;
+        for (var i = 1; i <= wheel.segments.length; i++) {
+          var angle = PI2 * (i / wheel.segments.length) + wheel.angleCurrent;
           wheel.drawSegment(i - 1, lastAngle, angle);
           lastAngle = angle;
         }
         // Draw a center circle
         ctx.beginPath();
-        ctx.arc(centerX, centerY, 200, 0, PI2, false);
+        ctx.arc(wheel.centerX, wheel.centerY, 200, 0, PI2, false);
         ctx.closePath();
 
         ctx.fillStyle = '#ffffff';
@@ -260,7 +337,7 @@
 
         // Draw outer circle
         ctx.beginPath();
-        ctx.arc(centerX, centerY, size, 0, PI2, false);
+        ctx.arc(wheel.centerX, wheel.centerY, wheel.size, 0, PI2, false);
         ctx.closePath();
 
         ctx.lineWidth = 10;
